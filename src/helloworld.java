af128184b78f62089ac984c8d372f3db56d84323
shamrockzhao <shamrock_zhao@hotmail.com>
2014-01-05 11:42:40 +0800

    修改目录结构

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,7 +3,7 @@ public class helloworld
 {
     public static void main(String[] args)
     {
-        System.out.printline("Hello World for java!");
-        System.out.printline("OK!");
+        System.out.println("Hello World for java!");
+        System.out.println("OK!");
     }
 }
